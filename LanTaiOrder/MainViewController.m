525eb04a01936dd5ff6f7e5309a911b65a245fbb
Ruby <alecchyi@gmail.com>
2013-01-25 12:59:31 +0800

    1

--- LanTaiOrder/MainViewController.m
+++ LanTaiOrder/MainViewController.m
@@ -27,7 +27,6 @@
 {
     self.navigationController.navigationBar.hidden = NO;
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
 }
 
 - (void)didReceiveMemoryWarning
